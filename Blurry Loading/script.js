cc415150ed614306fbd57cc6ce35ef087025ad1b
Kyle Buttress <75094144+commit-kyle@users.noreply.github.com>
2021-06-21 17:36:11 +0200

    Add files via upload

--- Blurry Loading/script.js
+++ Blurry Loading/script.js
@@ -1,23 +1,33 @@
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
+const btn = document.querySelector('.btn');
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
 
 let load = 0;
 
-let interval = setInterval(blurring, 30);
-
-function blurring() {
-  load++;
-
-  if (load > 99) {
-    clearInterval(interval);
-  }
-
-  loadText.innerText = `${load}%`;
-  loadText.style.opacity = scale(load, 0, 100, 1, 0);
-
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+if (load === 0) {
+	loadText.innerText = `Click to clear up`;
 }
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+const blurring = () => {
+	load++;
+
+	if (load < 100 && load > 0) {
+		loadText.innerText = `Clearing up - ${load}%`;
+		clearInterval(interval);
+	} else if (load > 99 && load < 101) {
+		loadText.innerText = `${load}% cleared up!`;
+    btn.style.display = 'none';
+    loadText.style.borderLeft ='none';
+	} else {
+		return;
+	}
+
+	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
+btn.addEventListener('click', () => {
+	let interval = setInterval(blurring, 40);
+});
